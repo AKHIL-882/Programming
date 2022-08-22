ecf070a45aa39604f2fa0337c4c392145b5bd176
Duggirala Akhil <51040667+AKHIL-882@users.noreply.github.com>
2022-08-22 12:09:31 +0530

    Delete Huddle Race.py

--- Array/Huddle Race.py
+++
@@ -1,9 +0,0 @@
-#huddle race
-array_size = int(input())
-height_person = int(input())
-height_array = [int(n) for n in input().split()]
-res = max(height_array) - height_person)
-if(res):
-    print(res)
-else:
-    print(0)
